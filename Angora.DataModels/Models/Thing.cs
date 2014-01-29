c460fa6a1c96b4ff4dba18681e3940f053fcbdcf
Michael Hollman <hollyfrass@gmail.com>
2014-01-28 18:30:46 -0600

    initial empty projects with Hello World

---
+++ Angora.DataModels/Models/Thing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Angora.DataModels.Interfaces;
+
+
+namespace Angora.DataModels.Models
+{
+    class Thing : IThing
+    {
+    }
+}
---
+++ Angora.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Angora.Web.MvcApplication" Language="C#" %>
