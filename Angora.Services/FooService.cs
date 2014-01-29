b041f8d9446f5e3b9d8aba25d8ba7b4ee71b1152
Michael Hollman <hollyfrass@gmail.com>
2014-01-29 01:04:47 -0600

    #3 added services project with service manager. pulled out data implementation as I don't know what ORM I want to use yet

---
+++ Angora.Services/FooService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Angora.Services
+{
+    public class FooService : IFooService
+    {
+        public string DoSomething(object value)
+        {
+            return string.Format("You did something to {0}", value.ToString());
+        }
+    }
+}
---
+++ Angora.Services/ServiceManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Angora.Services
+{
+    public static class ServiceManager
+    {
+
+        private static Dictionary<Type, object> Services;
+        private static Dictionary<Type, Constructor> Constructors;
+        private delegate object Constructor();
+
+        static ServiceManager()
+        {
+            Services = new Dictionary<Type, object>();
+            Constructors = new Dictionary<Type, Constructor>();
+
+            Constructors.Add(typeof(IFooService), () => new FooService());
+
+        }
+
+        public static T GetService<T>()
+        {
+            InitService<T>();
+            return (T)Services[typeof(T)];
+        }
+
+        private static void InitService<T>()
+        {
+            Type t = typeof(T);
+            if (!Services.ContainsKey(t))
+            {
+                if (Constructors.ContainsKey(t))
+                {
+                    Services.Add(t, Constructors[t].DynamicInvoke());
+                }
+            }
+        }
+
+    }
+}
