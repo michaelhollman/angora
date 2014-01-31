eeabed9860e4e0d464589ac369c3f532c66a52b0
Michael Hollman <hollyfrass@gmail.com>
2014-01-30 18:26:10 -0600

    Added api area

---
+++ Angora.Web/Areas/Api/Controllers/ApiBaseController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Angora.Web.Areas.Api.Controllers
+{
+    [RouteArea("api")]
+    public abstract class ApiBaseController : Controller
+    {
+	}
+}
