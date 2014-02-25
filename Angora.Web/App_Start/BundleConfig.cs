fa9739ecd4e68876641fff4760cb2546c9fd3aff
Michael Hollman <hollyfrass@gmail.com>
2014-02-25 17:09:04 -0600

    Added bootstrap, bootswatch, etc.

--- Angora.Web/App_Start/BundleConfig.cs
+++ Angora.Web/App_Start/BundleConfig.cs
@@ -23,9 +23,8 @@ namespace Angora.Web
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/bundles/angora").Include(
+                      "~/Styles/angora.css"));
         }
     }
 }
