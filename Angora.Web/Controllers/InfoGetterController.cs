a5564a72616a3f79ae658f67e79c19693be7afd4
Alex Bainter <alexbainter@gmail.com>
2014-02-25 22:48:18 -0600

    Initial work on pulling Facebook profile info for realz

---
+++ Angora.Web/Controllers/InfoGetterController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Angora.Data;
+using Angora.Data.Models;
+using Angora.Web.Models;
+using Facebook;
+
+namespace Angora.Web.Controllers
+{
+    public class InfoGetterController : Controller 
+    {
+        
+        public ActionResult Index()
+        {
+            InfoGetterModel model = new InfoGetterModel();
+            model.UserID = User.Identity.Name;
+            
+            return View(model);
+        }
+    }
+}
---
+++ Angora.Web/Models/InfoGettersModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Angora.Web.Models
+{
+    public class InfoGetterModel
+    {
+        public string UserID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set;}
+
+    }
+}
