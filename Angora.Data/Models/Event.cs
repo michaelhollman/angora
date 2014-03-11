72ae66e293a058593a2affadbcad270ce7676e77
Rachael <radahl01pps@gmail.com>
2014-03-11 13:51:21 -0500

    Some stuff

---
+++ Angora.Data/Models/Event.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Angora.Data.Models
+{
+    public class Event
+    {
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        //latitude then longitude
+        public string Location { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public List<string> Tags { get; set; }
+
+        [Required]
+        public DateTime CreationTime { get; set; }
+
+    }
+}
