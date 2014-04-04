a9b907c0ec2ba8c70bd5214a0e7b09117c124018
metalex9 <alexbainter@gmail.com>
2014-04-04 15:04:17 -0500

    I have no idea what I'm doing

---
+++ Angora.Web/Controllers/APIController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Angora.Data;
+
+
+namespace Angora.Web.Controllers
+{
+    public class APIController : ApiController
+    {
+        [AllowAnonymous]
+        [Route("login")]
+        public IHttpActionResult AttemptLogin(string loginId, string provider)
+        {
+            
+        }
+    }
+}
