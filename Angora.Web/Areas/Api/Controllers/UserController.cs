11ef2dc32d74d4db6bfa0cd23d094cb0269a025b
Alex Bainter <alexbainter@gmail.com>
2014-04-09 18:19:05 -0500

    Adding UserController (nothing in it right now)

---
+++ Angora.Web/Areas/Api/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Angora.Web.Areas.Api.Controllers
+{
+    public class UserController : ApiController
+    {
+        // GET api/<controller>/login
+        [HttpGet]
+        [Route("login")]
+        public string Get(string provider, string id, string accessToken)
+        {
+
+            return "value";
+        }
+
+    }
+}
