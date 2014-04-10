74000d7217c3e9e3ccd9b93e8d2227acbf87d03a
metalex9 <alexbainter@gmail.com>
2014-04-10 15:41:38 -0500

    Switching Machines

--- Angora.Web/Areas/Api/Controllers/UserController.cs
+++ Angora.Web/Areas/Api/Controllers/UserController.cs
@@ -4,18 +4,27 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Angora.Services;
+using Microsoft.AspNet.Identity;
 
 namespace Angora.Web.Areas.Api.Controllers
 {
+    [RoutePrefix("api/user")]
     public class UserController : ApiController
     {
+        private IAngoraUserService _userService;
+        public UserController(IAngoraUserService userService)
+        {
+            _userService = userService;
+        }
+
         // GET api/<controller>/login
         [HttpGet]
         [Route("login")]
-        public string Get(string provider, string id, string accessToken)
+        public string Get(string provider, string userId)
         {
-
-            return "value";
+            var user = _userService.FindUser(new UserLoginInfo(provider, userId));
+            return user.Id;
         }
 
     }
