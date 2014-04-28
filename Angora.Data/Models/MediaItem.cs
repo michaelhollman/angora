0f854ecea77586eac3e80e6f654895f78b81af58
Michael Hollman <hollyfrass@gmail.com>
2014-04-28 01:44:30 -0500

    Added pocos

---
+++ Angora.Data/Models/MediaItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Angora.Data.Models
+{
+    public class MediaItem : BaseModel
+    {
+        public string FooCDNBlob { get; set; }
+        public MediaType MediaType { get; set; }
+    }
+
+    public enum MediaType
+    {
+        Photo = 1,
+        Video = 2,
+        Other = -1
+    }
+}
---
+++ Angora.Data/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Angora.Data.Models
+{
+    public class Post : BaseModel
+    {
+        public virtual AngoraUser User { get; set; }
+        public virtual MediaItem MediaItem { get; set; }
+        public DateTime PostTime { get; set; }
+        public string PostText { get; set; }
+    }
+}
