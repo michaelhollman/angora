8d918f2d38fc33a1fffdfdc35991677e5c9d097d
metalex9 <alexbainter@gmail.com>
2014-05-01 01:11:23 -0500

    Removed some imports

--- Angora.Android/Angora/angora/src/main/java/com/angora/angora/SnapAndGoBuiltIn.java
+++ Angora.Android/Angora/angora/src/main/java/com/angora/angora/SnapAndGoBuiltIn.java
@@ -15,8 +15,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import java.io.ByteArrayOutputStream;
